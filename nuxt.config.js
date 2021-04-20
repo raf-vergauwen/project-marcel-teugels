11dacee76e8fae87c08657654aa420ca8c4e827d
Raf Vergauwen <vergauwen.r@outlook.com>
2021-04-20 11:59:49 +0200

    Main: Module Anime.js added

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
+    'nuxt-animejs',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
