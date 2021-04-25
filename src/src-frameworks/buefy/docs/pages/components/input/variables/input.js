a1dc603944b26704f83bf5bf8a0d50406a63dc76
Raf Vergauwen <vergauwen.r@outlook.com>
2021-04-25 20:09:33 +0200

    readme - small hotfix

--- src/src-frameworks/buefy/docs/pages/components/input/variables/input.js
+++
@@ -1,6 +0,0 @@
-export default [
-    {
-        name: 'Bulma variables',
-        default: '<a target="_blank" href="https://bulma.io/documentation/form/input/#variables">Link</a>'
-    }
-]
--- src/src-frameworks/buefy/docs/pages/components/progress/variables/progress.js
+++
@@ -1,6 +0,0 @@
-export default [
-    {
-        name: 'Bulma variables',
-        default: '<a target="_blank" href="https://bulma.io/documentation/elements/progress/#variables">Link</a>'
-    }
-]
