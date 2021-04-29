0a79811d99419d4e276a73d2e229036fa752080c
Raf Vergauwen <vergauwen.r@outlook.com>
2021-04-29 15:39:27 +0200

    style-main: assets folder v3

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/styles/scss/main.scss'],
+  css: ['bulma/css/bulma.css', '~/assets/styles/scss/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
