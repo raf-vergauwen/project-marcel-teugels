e8b40d096e356cdb6e99c45eb162f5369cbecee9
Raf Vergauwen <vergauwen.r@outlook.com>
2021-05-05 00:12:56 +0200

    style-main: color toggle almost works

---
+++ assets/scripts/javascript/webshop/webshop.js
@@ -0,0 +1,24 @@
+const $toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]',
+);
+const $currentTheme = localStorage.getItem('theme');
+
+if ($currentTheme) {
+  document.documentElement.setAttribute('data-theme', $currentTheme);
+
+  if ($currentTheme === 'dark') {
+    $toggleSwitch.checked = true;
+  }
+}
+
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+$toggleSwitch.addEventListener('change', switchTheme, false);
