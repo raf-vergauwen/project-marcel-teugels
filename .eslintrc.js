29c8ca68853a2aa24017ab2bef866da4d5a7010e
Raf Vergauwen <vergauwen.r@outlook.com>
2021-05-23 04:31:49 +0200

    development-main: bug fixing

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,29 @@ module.exports = {
     'eslint:recommended',
     'plugin:prettier/recommended',
   ],
-  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['@babel', 'vue', 'prettier'],
+  plugins: ['vue', 'prettier'],
 };
+
+// module.exports = {
+//   root: true,
+//   env: {
+//     browser: true,
+//     es2021: true,
+//     node: true,
+//   },
+//   extends: [
+//     'plugin:vue/recommended',
+//     'eslint:recommended',
+//     'plugin:prettier/recommended',
+//   ],
+//   parser: '@babel/eslint-parser',
+//   parserOptions: {
+//     ecmaVersion: 12,
+//     sourceType: 'module',
+//   },
+//   plugins: ['@babel', 'vue', 'prettier'],
+// };
